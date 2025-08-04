53a5c237d6e1d112468bf9424dac518deac6c9f8
Yaroslav Guz <aroslavguz@gmail.com>
2025-08-04 13:23:12 +0300

    Delete code dublication in cookie utils

--- auth-service/src/main/java/org/example/authservice/security/CookieUtils.java
+++ auth-service/src/main/java/org/example/authservice/security/CookieUtils.java
@@ -13,18 +13,27 @@ public class CookieUtils {
     @Value("${app.security.jwt.access-token-expiration}")
     private Long accessTokenExpiration;
 
-    public void addJwtCookies(HttpServletResponse response, String accessToken) {
-        Cookie accessCookie = new Cookie(ACCESS_TOKEN_COOKIE, accessToken);
-        accessCookie.setHttpOnly(true);
-        accessCookie.setSecure(true);
-        accessCookie.setPath("/");
-        accessCookie.setMaxAge((int) (accessTokenExpiration / 1000));
+    public void addAccessTokenCookie(HttpServletResponse response, String accessToken) {
+        Cookie cookie = createCookie(ACCESS_TOKEN_COOKIE, accessToken, (int) (accessTokenExpiration / 1000));
+        response.addCookie(cookie);
+    }
 
-        response.addCookie(accessCookie);
+    public void clearAccessTokenCookie(HttpServletResponse response) {
+        Cookie cookie = createCookie(ACCESS_TOKEN_COOKIE, null, 0);
+        response.addCookie(cookie);
     }
 
     public String getAccessToken(HttpServletRequest request) {
         return getCookieValue(request, ACCESS_TOKEN_COOKIE);
+    }
+
+    private Cookie createCookie(String name, String value, int maxAge) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(maxAge);
+        return cookie;
     }
 
     private String getCookieValue(HttpServletRequest request, String name) {
