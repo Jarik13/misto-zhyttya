00777908fbc4e84991bad7e99b6a0ce01e9e1a31
Yaroslav Guz <aroslavguz@gmail.com>
2025-08-04 16:44:59 +0300

    Add swagger schema example to login request

--- auth-service/src/main/java/org/example/authservice/dto/auth/LoginRequest.java
+++ auth-service/src/main/java/org/example/authservice/dto/auth/LoginRequest.java
@@ -1,14 +1,17 @@
 package org.example.authservice.dto.auth;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record LoginRequest(
+        @Schema(description = "User email", example = "user@example.com")
         @NotBlank(message = "Email must not be empty")
         @Email(message = "Email should be valid")
         String email,
 
+        @Schema(description = "User password", example = "Password123!")
         @NotBlank(message = "Password must not be empty")
         @Size(min = 6, message = "Password must be at least 6 characters")
         String password
