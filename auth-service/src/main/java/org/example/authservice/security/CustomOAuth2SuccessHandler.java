cf0cc0fb70c5041e11f3dc9a103a0180adba76ff
Yaroslav Guz <aroslavguz@gmail.com>
2025-08-05 23:13:09 +0300

    Create custom oauth2 success handler

---
+++ auth-service/src/main/java/org/example/authservice/security/CustomOAuth2SuccessHandler.java
@@ -0,0 +1,73 @@
+package org.example.authservice.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.example.authservice.model.AuthProvider;
+import org.example.authservice.model.Role;
+import org.example.authservice.model.User;
+import org.example.authservice.repository.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class CustomOAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    private final CookieUtils cookieUtils;
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        String email = oAuth2User.getAttribute("email");
+
+        String registrationId = ((OAuth2AuthenticationToken) authentication)
+                .getAuthorizedClientRegistrationId()
+                .toUpperCase();
+
+        AuthProvider provider;
+        try {
+            provider = AuthProvider.valueOf(registrationId);
+        } catch (IllegalArgumentException e) {
+            provider = AuthProvider.LOCAL;
+        }
+
+        final AuthProvider finalProvider = provider;
+        var appUser = userRepository.findByEmailIgnoreCase(email)
+                .orElseGet(() -> userRepository.save(
+                        User.builder()
+                                .email(email)
+                                .password(null)
+                                .role(Role.USER)
+                                .provider(finalProvider)
+                                .enabled(true)
+                                .build()
+                ));
+
+        UUID userId = appUser.getId();
+
+        cookieUtils.addAccessTokenCookie(
+                response,
+                jwtService.generateAccessToken(email, userId.toString())
+        );
+
+        cookieUtils.addRefreshTokenCookie(
+                response,
+                jwtService.generateRefreshToken(email, userId.toString())
+        );
+
+        String targetUrl = "http://localhost:5173";
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
+
+}
