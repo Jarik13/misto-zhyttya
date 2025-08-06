df732bcd4dc50cdab5c612eda49fd979330cd6fd
Yaroslav Guz <aroslavguz@gmail.com>
2025-08-06 10:26:49 +0300

    Create default oauth2 user info strategy

---
+++ auth-service/src/main/java/org/example/authservice/security/provider/DefaultOAuth2UserInfoProcessor.java
@@ -0,0 +1,16 @@
+package org.example.authservice.security.provider;
+
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component("default")
+public class DefaultOAuth2UserInfoProcessor implements OAuth2UserInfoProcessor {
+    @Override
+    public Map<String, Object> enrichAttributes(OAuth2UserRequest userRequest, OAuth2User oAuth2User) {
+        return new HashMap<>(oAuth2User.getAttributes());
+    }
+}
