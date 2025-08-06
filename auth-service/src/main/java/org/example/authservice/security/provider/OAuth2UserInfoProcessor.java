29040d4d3e8d236c33edb3e5b540fc7dee3ad2ea
Yaroslav Guz <aroslavguz@gmail.com>
2025-08-06 10:24:00 +0300

    Create oauth2 user info strategy

---
+++ auth-service/src/main/java/org/example/authservice/security/provider/OAuth2UserInfoProcessor.java
@@ -0,0 +1,10 @@
+package org.example.authservice.security.provider;
+
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Map;
+
+public interface OAuth2UserInfoProcessor {
+    Map<String, Object> enrichAttributes(OAuth2UserRequest userRequest, OAuth2User oAuth2User);
+}
