dc1beba1c2af524510f308cb871b46dadb462ae6
Yaroslav Guz <aroslavguz@gmail.com>
2025-08-08 10:57:23 +0300

    Update gender enum

--- user-profile-service/src/main/java/org/example/userprofileservice/model/Gender.java
+++ user-profile-service/src/main/java/org/example/userprofileservice/model/Gender.java
@@ -1,5 +1,26 @@
 package org.example.userprofileservice.model;
 
+import lombok.Getter;
+
+@Getter
 public enum Gender {
-    MALE, FEMALE, TRANSGENDER, OTHER
+    MALE(0),
+    FEMALE(1),
+    TRANSGENDER(2),
+    OTHER(3);
+
+    private final long id;
+
+    Gender(long id) {
+        this.id = id;
+    }
+
+    public static Gender fromId(long id) {
+        for (Gender g : values()) {
+            if (g.id == id) {
+                return g;
+            }
+        }
+        throw new IllegalArgumentException("Unknown gender id: " + id);
+    }
 }
