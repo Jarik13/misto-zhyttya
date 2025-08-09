9c95e89e37d8e5ae388ffbf09b3050cd717124fd
Yaroslav Guz <aroslavguz@gmail.com>
2025-08-09 10:17:16 +0300

    Fix bug with user profile mapper

--- user-profile-service/src/main/java/org/example/userprofileservice/mapper/UserProfileMapper.java
+++ user-profile-service/src/main/java/org/example/userprofileservice/mapper/UserProfileMapper.java
@@ -9,13 +9,17 @@ import java.time.format.DateTimeFormatter;
 
 public class UserProfileMapper {
     public static Profile toUserProfile(CreateUserProfileRequest request) {
+        LocalDate dateOfBirth = null;
+        String dobString = request.getDateOfBirth();
+
+        if (!dobString.isEmpty()) {
+            dateOfBirth = LocalDate.parse(dobString, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        }
+
         return Profile.builder()
                 .username(request.getUsername())
                 .phoneNumber(request.getPhoneNumber())
-                .dateOfBirth(LocalDate.parse(
-                        request.getDateOfBirth(),
-                        DateTimeFormatter.ofPattern("yyyy-MM-dd")
-                ))
+                .dateOfBirth(dateOfBirth)
                 .gender(Gender.fromId(request.getGenderId()))
                 .avatarUrl(request.getAvatarUrl())
                 .userId(request.getUserId())
