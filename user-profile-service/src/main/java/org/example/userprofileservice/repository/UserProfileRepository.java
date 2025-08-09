093ad1a5bb5062ef54fcb87fcb0873663f084e7c
Yaroslav Guz <aroslavguz@gmail.com>
2025-08-09 10:37:16 +0300

    Update getting existing user profile

--- user-profile-service/src/main/java/org/example/userprofileservice/repository/UserProfileRepository.java
+++ user-profile-service/src/main/java/org/example/userprofileservice/repository/UserProfileRepository.java
@@ -4,9 +4,11 @@ import org.example.userprofileservice.model.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserProfileRepository extends JpaRepository<Profile, UUID> {
     boolean existsByPhoneNumber(String phoneNumber);
+    Optional<Profile> findByUserId(String userId);
 }
