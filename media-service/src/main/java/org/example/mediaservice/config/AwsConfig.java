7aba0d75f09e09682d1e3ce76b73b60c0f5530b0
Yaroslav Guz <aroslavguz@gmail.com>
2025-08-10 16:25:54 +0300

    Update aws config in media service

--- media-service/src/main/java/org/example/mediaservice/config/AwsConfig.java
+++ media-service/src/main/java/org/example/mediaservice/config/AwsConfig.java
@@ -6,16 +6,15 @@ import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class AwsConfig {
     @Bean
     public S3Client s3Client(
-            @Value("${aws.s3.access-key}")
-            String accessKey,
-
-            @Value("${aws.s3.secret-key}")
-            String secretKey
+            @Value("${aws.s3.access-key}") String accessKey,
+            @Value("${aws.s3.secret-key}") String secretKey,
+            @Value("${aws.s3.region}") String region
     ) {
         AwsBasicCredentials credentials = AwsBasicCredentials.builder()
                 .accessKeyId(accessKey)
@@ -24,7 +23,14 @@ public class AwsConfig {
 
         return S3Client.builder()
                 .credentialsProvider(() -> credentials)
-                .region(Region.EU_NORTH_1)
+                .region(Region.of(region))
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner(@Value("${aws.s3.region}") String region) {
+        return S3Presigner.builder()
+                .region(Region.of(region))
                 .build();
     }
 }
