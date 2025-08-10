ece85e3b8aba6cf7691160f2a718afb204b77a29
Yaroslav Guz <aroslavguz@gmail.com>
2025-08-10 16:17:05 +0300

    Create media service

---
+++ media-service/src/main/java/org/example/mediaservice/service/MediaService.java
@@ -0,0 +1,10 @@
+package org.example.mediaservice.service;
+
+import org.example.mediaservice.dto.MediaResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface MediaService {
+    MediaResponse uploadMedia(MultipartFile multipartFile) throws IOException;
+}
