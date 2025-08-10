d471bd939a4c40c1ec1d49dc8b8c7148530073f0
Yaroslav Guz <aroslavguz@gmail.com>
2025-08-10 10:59:22 +0300

    Add swagger schema to profile response

--- user-profile-service/src/main/java/org/example/userprofileservice/dto/profile/ProfileResponse.java
+++ user-profile-service/src/main/java/org/example/userprofileservice/dto/profile/ProfileResponse.java
@@ -1,12 +1,23 @@
 package org.example.userprofileservice.dto.profile;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDate;
 
 public record ProfileResponse(
+        @Schema(description = "Username", example = "john_doe")
         String username,
+
+        @Schema(description = "Phone number in international format", example = "+380991234567")
         String phoneNumber,
+
+        @Schema(description = "Date of birth in format yyyy-MM-dd", example = "2000-01-01")
         LocalDate dateOfBirth,
+
+        @Schema(description = "Gender ID (0 - male, 1 - female, 2 - transgender, 3 - not specified)", example = "0")
         Long genderId,
+
+        @Schema(description = "URL to the user's avatar", example = "https://example.com/avatar.jpg")
         String avatarUrl
 ) {
 }
