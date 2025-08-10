ef6de6f48b3a0a8b6842a2fea111bf8c2576578d
Yaroslav Guz <aroslavguz@gmail.com>
2025-08-10 15:19:20 +0300

    Create media repository

---
+++ media-service/src/main/java/org/example/mediaservice/repository/MediaRepository.java
@@ -0,0 +1,12 @@
+package org.example.mediaservice.repository;
+
+import org.example.mediaservice.model.Media;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MediaRepository extends MongoRepository<Media, String> {
+    Optional<Media> findByUrl(String url);
+}
