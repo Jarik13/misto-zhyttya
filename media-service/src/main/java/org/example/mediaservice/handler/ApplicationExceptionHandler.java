341c57c7e6f598f9c37e8a59de30d2f85c6cee69
Yaroslav Guz <aroslavguz@gmail.com>
2025-08-12 16:08:23 +0300

    Add exception handling when file is largest then max file size

--- media-service/src/main/java/org/example/mediaservice/handler/ApplicationExceptionHandler.java
+++ media-service/src/main/java/org/example/mediaservice/handler/ApplicationExceptionHandler.java
@@ -4,10 +4,12 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.example.mediaservice.dto.error.ErrorResponse;
 import org.example.mediaservice.exception.BusinessException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import static org.example.mediaservice.dto.error.ErrorCode.INTERNAL_EXCEPTION;
 import static org.example.mediaservice.dto.error.ErrorCode.RESOURCE_NOT_FOUND;
@@ -16,6 +18,9 @@ import static org.example.mediaservice.dto.error.ErrorCode.RESOURCE_NOT_FOUND;
 @Slf4j
 @RestControllerAdvice
 public class ApplicationExceptionHandler {
+    @Value("${spring.servlet.multipart.max-file-size}")
+    private String maxFileSize;
+
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException exception) {
         log.warn("Business exception: {}", exception.getMessage());
@@ -30,6 +35,20 @@ public class ApplicationExceptionHandler {
                 : HttpStatus.BAD_REQUEST;
 
         return new ResponseEntity<>(response, status);
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<ErrorResponse> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException exception) {
+        log.warn("Upload failed: file size exceeded the maximum limit");
+
+        String message = String.format("Розмір файлу перевищує дозволений ліміт: %s", maxFileSize);
+
+        ErrorResponse response = ErrorResponse.builder()
+                .code("FILE_TOO_LARGE")
+                .message(message)
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.PAYLOAD_TOO_LARGE);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
