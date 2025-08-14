4bfd176d81b3ac46dd2a161d7241ef5f3db319c7
Yaroslav Guz <aroslavguz@gmail.com>
2025-08-14 10:58:07 +0300

    Update user profile consumer

--- media-service/src/main/java/org/example/mediaservice/kafka/UserProfileConsumer.java
+++ media-service/src/main/java/org/example/mediaservice/kafka/UserProfileConsumer.java
@@ -2,8 +2,9 @@ package org.example.mediaservice.kafka;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.example.mediaservice.service.MediaService;
+import org.example.mediaservice.dto.media.AvatarEvent;
+import org.example.mediaservice.kafka.strategy.AvatarEventHandlerFactory;
+import org.example.mediaservice.model.Status;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -11,17 +12,17 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserProfileConsumer {
-    private final MediaService mediaService;
+    private final AvatarEventHandlerFactory avatarEventHandlerFactory;
 
-    @KafkaListener(topics = "user-profile-delete-avatar", groupId = "media-service")
-    public void consumeDeletionUserAvatar(String key) {
-        log.info("Received request to delete avatar with key: {}", key);
+    @KafkaListener(topics = "user-profile-avatar-events", groupId = "media-service")
+    public void consumeAvatarEvent(String action, String key) {
+        log.info("Received avatar event: action={}, key={}", action, key);
 
         try {
-            mediaService.deleteMediaByKey(key);
-            log.info("Successfully deleted media with key: {}", key);
+            avatarEventHandlerFactory.getHandler(Status.valueOf(action)).handle(new AvatarEvent(action, key));
+            log.info("Successfully consumed avatar event");
         } catch (Exception ex) {
-            log.error("Failed to delete media with key {}: {}", key, ex.getMessage());
+            log.error("Failed to do avatar event with key {}: {}", key, ex.getMessage());
         }
     }
 }
