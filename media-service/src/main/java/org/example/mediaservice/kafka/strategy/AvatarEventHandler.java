a8a4638852619404bf3e5ded19e8a422fa0a2f2e
Yaroslav Guz <aroslavguz@gmail.com>
2025-08-14 10:53:25 +0300

    Create factory for getting required avatar handler

--- media-service/src/main/java/org/example/mediaservice/kafka/strategy/AvatarEventHandler.java
+++ media-service/src/main/java/org/example/mediaservice/kafka/strategy/AvatarEventHandler.java
@@ -1,7 +1,9 @@
 package org.example.mediaservice.kafka.strategy;
 
 import org.example.mediaservice.dto.media.AvatarEvent;
+import org.example.mediaservice.model.Status;
 
 public interface AvatarEventHandler {
     void handle(AvatarEvent event);
+    Status getStatus();
 }
--- media-service/src/main/java/org/example/mediaservice/kafka/strategy/impl/UpdateAvatarStatusHandler.java
+++ media-service/src/main/java/org/example/mediaservice/kafka/strategy/impl/UpdateAvatarStatusHandler.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.mediaservice.dto.media.AvatarEvent;
 import org.example.mediaservice.kafka.strategy.AvatarEventHandler;
+import org.example.mediaservice.model.Status;
 import org.example.mediaservice.service.MediaService;
 import org.springframework.stereotype.Service;
 
@@ -17,5 +18,10 @@ public class UpdateAvatarStatusHandler implements AvatarEventHandler {
     public void handle(AvatarEvent event) {
         mediaService.updateMediaStatus(event.action(), event.key());
         log.info("Updated media status for key {} to {}", event.key(), event.action());
+    }
+
+    @Override
+    public Status getStatus() {
+        return Status.APPROVED;
     }
 }
